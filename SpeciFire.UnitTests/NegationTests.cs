8c5b8547503d1a18589e4ba644a47d1ee871a195
Kusken <jallowski@hotmail.se>
2017-09-09 10:54:25 +0200

    Added functionality: Specification compositions can now be negated as a whole

--- SpeciFire.UnitTests/NegationTests.cs
+++ SpeciFire.UnitTests/NegationTests.cs
@@ -7,7 +7,7 @@ namespace SpeciFire.UnitTests
     public class NegationTests
     {
         [Fact]
-        public void Given_IsPSpecification_When_IsPSpecificationIsNegated_And_ToExpressionIsCalled_Then_Expression_NegationPIsReturned()
+        public void Given_IsPSpecification_When_IsPSpecificationIsNegated_And_ToExpressionIsCalled_Then_Expression_NegationIsP_IsReturned()
         {
             var isPSpecification = Given.PropositionSpecification.IsPStub().Build();
 
@@ -15,7 +15,38 @@ namespace SpeciFire.UnitTests
             var predicate = isPSpecification.NOT.ToExpression();
 
 
-            predicate.ToString().Should().Be("x => Not(x.P)", "because they are equivalent");
+            predicate.ToString().Should().Be("x => Not(x.P)");
+        }
+
+
+        [Fact]
+        public void Given_IsPSpecification_And_IsQSpecification_When_BothAreNegatedSeparately_And_SpecifiedAsAConjunction_When_ToExpressionIsCalled_Then_Expression_NegationIsPAndNegationIsQ_IsReturned()
+        {
+            var isPSpecification = Given.PropositionSpecification.IsPStub().Build();
+            var isQSpecification = Given.PropositionSpecification.IsQStub().Build();
+            var initialSpecificationSut = Specification<IProposition>.Initialize;
+
+            var predicate = initialSpecificationSut.Specify.From(isPSpecification.NOT).AND(isQSpecification.NOT)
+                .ToExpression();
+
+
+            predicate.ToString().Should().Be("x => (Not(x.P) AndAlso Not(x.Q))");
+        }
+
+
+        [Fact]
+        public void Given_IsPSpecification_And_IsQSpecification_When_SpecifiedAsAConjunction_And_ConjunctionIsNegated_When_ToExpressionIsCalled_Then_Expression_Negation_IsPAndIsQ_IsReturned()
+        {
+            var isPSpecification = Given.PropositionSpecification.IsPStub().Build();
+            var isQSpecification = Given.PropositionSpecification.IsQStub().Build();
+            var initialSpecificationSut = Specification<IProposition>.Initialize;
+
+
+            var predicate = initialSpecificationSut.Specify.From(isPSpecification).AND(isQSpecification).NOT
+                .ToExpression();
+
+
+            predicate.ToString().Should().Be("x => Not((x.P AndAlso x.Q))");
         }
     }
 }
